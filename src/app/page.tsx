d955c8bba684ccee5e1a260cf00bcd29bc0cf101
Edqe14 <34704796+Edqe14@users.noreply.github.com>
2025-04-08 17:50:52 +0700

    chore: remove log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,8 +37,6 @@ export default function Home() {
     };
   }, []);
 
-  console.log(tags);
-
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   const images = useMemo(() => {
     return (
